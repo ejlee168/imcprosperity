55e1443a21fab33aa0fd24fe632b2877e9c96ff8
Ethan Lee <ejlee168@gmail.com>
2024-03-28 17:57:59 +1000

    Adds RSI prediction for amethyst, since amethyst doesn't flctuate in price much, the RSI always hovers around 50... fix this shit

--- tutorial_round/tutorial_algorithm.py
+++ tutorial_round/tutorial_algorithm.py
@@ -98,8 +98,11 @@ class Trader:
 
     # Amethyst cache to store amethysts
     amethyst_cache = []
+    amethyst_gain = 0
+    amethyst_loss = 0
     amethyst_time_cache = []
-    amethyst_cache_num = 23 # change this value to adjust the 'lag'
+    amethyst_cache_num = 14 # RSI generally operates on a 14* trade period 
+
 
     # Helper function to cache the midprice of a product
     def cache_product(self, product: Symbol, state: TradingState):
@@ -119,13 +122,58 @@ class Trader:
                 self.amethyst_cache.append((best_ask + best_bid)/2)
                 self.amethyst_time_cache.append(state.timestamp)
 
-    def get_RSI(self, state: TradingState):
-        
+    # Gets the gains and losses for a amethyst based on RSI algorithm
+    def get_amethyst_gains_losses(self, state: TradingState):
         '''
             GAIN: if current mid price > previous mid price = store currentprice - previous mid price
             LOSS: if current mid price < previous mid price = store previous mid price - currentprice
         '''
-        return
+        order_depth: OrderDepth = state.order_depths["AMETHYSTS"]
+
+        best_ask, _ = list(order_depth.sell_orders.items())[0]
+        best_bid, _ = list(order_depth.buy_orders.items())[0]
+
+        mid_price = (best_ask + best_bid)/2
+
+        last_mid_price = self.amethyst_cache[len(self.amethyst_cache) - 2] # get the most previous midprice
+
+        logger.print("Amethyst mid price: ", mid_price)
+        logger.print("Amethyst last mid price: ", last_mid_price)
+
+        self.amethyst_gain = 0
+        self.amethyst_loss = 0
+
+        for i in range(len(self.amethyst_cache) - 1):
+            mid_price = self.amethyst_cache[i + 1]
+            last_mid_price = self.amethyst_cache[i]
+            if (mid_price > last_mid_price): 
+                self.amethyst_gain += mid_price - last_mid_price
+            else:
+                self.amethyst_loss += last_mid_price - mid_price
+
+        # if (mid_price > last_mid_price): 
+        #     self.amethyst_gain += mid_price - last_mid_price
+        # else:
+        #     self.amethyst_loss += last_mid_price - mid_price
+    
+    # Assumes that length is not 0
+    def get_amethyst_RSI(self):
+        amethyst_prices_stored = len(self.amethyst_cache)
+
+        amethyst_avg_gain = self.amethyst_gain / amethyst_prices_stored
+        amethyst_avg_loss = self.amethyst_loss / amethyst_prices_stored
+        logger.print("AMETHYST gain: ", amethyst_avg_gain, " ", amethyst_prices_stored)
+        logger.print("AMETHYST loss: ", amethyst_avg_loss, " ", amethyst_prices_stored)
+
+        if (amethyst_avg_loss == 0):
+            return 50 # default RSI if there is no loss -- should this be 50?!?
+        
+        RS = amethyst_avg_gain / amethyst_avg_loss
+
+        RSI = 100 - (100 / (1 + RS))
+
+        logger.print("AMETHYST RSI: ", RSI)
+        return RSI
 
     # Calculates regression when given the times and prices, and a timestamp to predict the price of
     def calculate_regression(self, times: list[int], prices: list[int], timestamp: int):
@@ -178,26 +226,18 @@ class Trader:
             best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
             best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
 
-            # Sets acceptable prices to buy
+            # Calculate RSI for amethysts
             if product == "AMETHYSTS": 
-                # Price is based on regression prediction of next timestamp, otherwise average sum
-                predicted_price = self.calculate_regression(self.amethyst_time_cache, self.amethyst_cache, state.timestamp + 100)
+                #acceptable_price = 10000
+                if (state.timestamp <= 100):
+                    amethyst_RSI = 50
+                else:
+                    logger.print("Ameth GAIN: ", self.amethyst_gain)
+                    logger.print("Ameth LOSS: ", self.amethyst_loss)
+                    self.get_amethyst_gains_losses(state)
+                    amethyst_RSI = self.get_amethyst_RSI()
 
-                # When the timestamp is not 0 and the price can be predicted with regression
-                if (state.timestamp != 0 and predicted_price != -1): 
-                    acceptable_price = round(predicted_price, 5)
-
-                elif (state.timestamp == 0): # When the timestamp is 0, set price to 5000
-                    acceptable_price = 10000
-
-                else: # when the  price cannot be predicted with regression, then use moving average midprice
-                    acceptable_price = round(sum(self.amethyst_cache)/self.amethyst_cache_num, 5)
-               
-                logger.print("Amethyst cache num", self.amethyst_cache_num)
-                logger.print("Amethyst acceptable price ", acceptable_price)
-                logger.print("Best ask: ", best_ask)
-                logger.print("Best bid: ", best_bid)
-
+            # Calculate prices for starfruit
             elif product == "STARFRUIT":
                 # Price is based on regression prediction of next timestamp, otherwise average sum
                 predicted_price = self.calculate_regression(self.starfruit_time_cache, self.starfruit_cache, state.timestamp + 100)
@@ -217,18 +257,32 @@ class Trader:
                 logger.print("Best ask: ", best_ask)
                 logger.print("Best bid: ", best_bid)
 
-            # Do the BUYING 
-            if len(order_depth.sell_orders) != 0:
-                if int(best_ask) < acceptable_price:
-                    logger.print(product, " BUY", str(-best_ask_amount) + "x", best_ask)
-                    orders.append(Order(product, best_ask, -best_ask_amount))
-    
-            # Do the SELLING
-            if len(order_depth.buy_orders) != 0:
-                if int(best_bid) > acceptable_price:
-                    logger.print(product, " SELL", str(best_bid_amount) + "x", best_bid)
-                    orders.append(Order(product, best_bid, -best_bid_amount))
-            
+            if product == "AMETHYSTS":
+                # Do the BUYING 
+                if len(order_depth.sell_orders) != 0:
+                    if amethyst_RSI < 30:
+                        logger.print(product, " BUY", str(-best_ask_amount) + "x", best_ask)
+                        orders.append(Order(product, best_ask, -best_ask_amount))
+        
+                # Do the SELLING
+                if len(order_depth.buy_orders) != 0:
+                    if amethyst_RSI > 70:
+                        logger.print(product, " SELL", str(best_bid_amount) + "x", best_bid)
+                        orders.append(Order(product, best_bid, -best_bid_amount))
+
+            elif product == "STARFRUIT":
+                # Do the BUYING 
+                if len(order_depth.sell_orders) != 0:
+                    if int(best_ask) < acceptable_price:
+                        logger.print(product, " BUY", str(-best_ask_amount) + "x", best_ask)
+                        orders.append(Order(product, best_ask, -best_ask_amount))
+        
+                # Do the SELLING
+                if len(order_depth.buy_orders) != 0:
+                    if int(best_bid) > acceptable_price:
+                        logger.print(product, " SELL", str(best_bid_amount) + "x", best_bid)
+                        orders.append(Order(product, best_bid, -best_bid_amount))
+
             # Add the orders of the corresponding product to result
             result[product] = orders
         
